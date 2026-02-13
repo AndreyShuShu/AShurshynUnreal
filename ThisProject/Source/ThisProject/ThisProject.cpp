4b0a70feef1f9cc60aed2983b67812fd8b137b1b
Andrii <positiveandrew777@gmail.com>
2026-02-13 23:12:46 +0200

    Homework #26

---
+++ ThisProject/Source/ThisProject/ThisProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThisProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThisProject, "ThisProject" );
